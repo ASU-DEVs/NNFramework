3c5013a0feb703280748285a05a8f5f7f068833f
andrewrfn <52424939+andrewrfn@users.noreply.github.com>
2020-12-30 07:47:02 +0200

    Update Preprocessing.py

--- Preprocessing/Preprocessing.py
+++ Preprocessing/Preprocessing.py
@@ -5,52 +5,58 @@ import pickle
 
 #Insert path of MNIST or CIFAR Datasets.
 
-path = 'D:\\Gam3a\\Neural\\Project\\Datasets\\MNIST'
-data_directory_path = path
-filenames = os.listdir(data_directory_path)
+
+def preprocessing(path):
+
+
+
+   # path = 'D:\\Gam3a\\Neural\\Project\\Datasets\\MNIST'
+    data_directory_path = path
+    filenames = os.listdir(data_directory_path)
 
 
 
 
-#For CIFAR Dataset:
+    #For CIFAR Dataset:
 
-if 'csv' not in filenames[0]:
+    if 'csv' not in filenames[0]:
 
-    trainLabels = []
-    trainFeatures = []
+        trainLabels = []
+        trainFeatures = []
 
-    for file in filenames:
-        if '_batch' in file:
-            with open(path+'/'+file, 'rb') as fo:
-                dict = pickle.load(fo, encoding='bytes')
-                if 'test' in file:
-                    testLabels = dict[b'labels']
-                    testFeatures = dict[b'data']
-                else:
-                    trainLabels.append(dict[b'labels'])
-                    trainFeatures.append(dict[b'data'])
+        for file in filenames:
+            if '_batch' in file:
+                with open(path+'/'+file, 'rb') as fo:
+                    dict = pickle.load(fo, encoding='bytes')
+                    if 'test' in file:
+                        testLabels = dict[b'labels']
+                        testFeatures = dict[b'data']
+                    else:
+                        trainLabels.append(dict[b'labels'])
+                        trainFeatures.append(dict[b'data'])
 
-    trainLabels = np.array(trainLabels)
-    trainFeatures = np.array(trainFeatures)
-    trainLabels = trainLabels.reshape((-1,1))
-    trainFeatures = trainFeatures.reshape((-1,3072))
-    testLabels = np.array(testLabels)
+        trainLabels = np.array(trainLabels)
+        trainFeatures = np.array(trainFeatures)
+        trainLabels = trainLabels.reshape((-1,1))
+        trainFeatures = trainFeatures.reshape((-1,3072))
+        testLabels = np.array(testLabels)
 
-#For MNIST Dataset:
+    #For MNIST Dataset:
 
-else:
-    trainData = pd.read_csv(path+'/train.csv')
-    testData = pd.read_csv(path+'/test.csv')
-    trainData = np.array(trainData)
-    trainLabels = np.array(trainData[:,0])
-    trainFeatures = np.array(trainData[:,1:])
-    testFeatures = np.array(testData)
-
-
-
+    else:
+        trainData = pd.read_csv(path+'/train.csv')
+        testData = pd.read_csv(path+'/test.csv')
+        trainData = np.array(trainData)
+        trainLabels = np.array(trainData[:,0])
+        trainFeatures = np.array(trainData[:,1:])
+        testFeatures = np.array(testData)
+        testLabels = np.zeros(5)  #No train labels given in dataset
 
 
 
+    return trainFeatures, trainLabels, testFeatures, testLabels
 
 
+
+preprocessing('D:\\Gam3a\\Neural\\Project\\Datasets\\CIFAR-10\\cifar-10-batches-py')
             
