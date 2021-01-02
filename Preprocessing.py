55c7779a8773a2b5ac1dc69377b71227034ea5ed
andrewrfn <52424939+andrewrfn@users.noreply.github.com>
2021-01-02 21:05:52 +0200

    Adding test labels

--- Preprocessing.py
+++ Preprocessing.py
@@ -5,7 +5,7 @@ import pickle
 
 def preprocessing(path):
 
-   # path = 'D:\\Gam3a\\Neural\\Project\\Datasets\\MNIST'
+    #path = 'D:\\Gam3a\\Neural\\Project\\Datasets\\MNIST'
     data_directory_path = path
     filenames = os.listdir(data_directory_path)
 
@@ -15,7 +15,7 @@ def preprocessing(path):
 
         trainLabels = []
         trainFeatures = []
-
+        Dataset = 'CIFAR10'
         for file in filenames:
             if '_batch' in file:
                 with open(path+'/'+file, 'rb') as fo:
@@ -39,12 +39,14 @@ def preprocessing(path):
         trainData = pd.read_csv(path+'/train.csv')
         testData = pd.read_csv(path+'/test.csv')
         trainData = np.array(trainData)
+        testData = np.array(testData)
         trainLabels = np.array(trainData[:,0])
         trainFeatures = np.array(trainData[:,1:])
-        testFeatures = np.array(testData)
-        testLabels = np.zeros(5)  #No train labels given in dataset
+        testFeatures = np.array(testData[:,1:])
+        testLabels = np.array(testData[:,0])
+        Dataset = 'MNIST'
 
-    return trainFeatures, trainLabels, testFeatures, testLabels
+    return trainFeatures, trainLabels, testFeatures, testLabels, Dataset
 
 
-preprocessing('D:\\Gam3a\\Neural\\Project\\Datasets\\CIFAR-10\\cifar-10-batches-py')
+#preprocessing(r'D:\Gam3a\Neural\Project\Datasets\MNIST')
