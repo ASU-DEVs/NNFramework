8e77e428015131e373fdc209375f3a500e153a15
andrewrfn <52424939+andrewrfn@users.noreply.github.com>
2021-01-25 01:50:22 +0200

    bug fix

--- Modules/CNN_Saver.py
+++ Modules/CNN_Saver.py
@@ -4,11 +4,18 @@ import os
 class CNN_Saver():
      
     def save(self,LeNet):
-        filename = 'saved_model_mnist.dat'
+        filename = 'saved_model.dat'
         pickle.dump(LeNet.get_params(), open(filename, 'wb'))
         print("file saved")
             
     def restore(self,filename):   
-        with open(os.getcwd() +'\\' +filename, 'rb') as handle:
-            unserialized_data = pickle.load(handle)
-        return unserialized_data
+       try:
+           with open(os.getcwd() +'/'+filename, 'rb') as handle:
+               unserialized_data = pickle.load(handle)
+               
+       except:
+           with open(os.getcwd() +'\\'+filename, 'rb') as handle:
+               unserialized_data = pickle.load(handle)
+
+               
+       return unserialized_data
