a4ae157fb2328ccdd44445a29c01595dc494305a
PassantElBaroudy <76267023+PassantElBaroudy@users.noreply.github.com>
2021-01-24 21:28:46 +0200

    Update actvfn.py

--- Modules/actvfn.py
+++ Modules/actvfn.py
@@ -29,11 +29,10 @@ def derivative_sigmoid(x):
 
 
 def derivative_tanh(x):
-   # f = 1-(tanh(x)*tanh(x))
-    f = 1.0 - np.tanh(x)**2
+    f = 1-(tanh(x)*tanh(x))
     return f
 
-    
+
 def d_linear(func):
     return np.ones(func.shape)
 
