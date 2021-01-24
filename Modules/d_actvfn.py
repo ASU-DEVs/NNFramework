5dfce666d6658a7081d3969f09edc3e062637546
PassantElBaroudy <76267023+PassantElBaroudy@users.noreply.github.com>
2021-01-24 15:23:13 +0200

    Update d_actvfn.py

--- Modules/d_actvfn.py
+++ Modules/d_actvfn.py
@@ -1,30 +1,3 @@
-import numpy as np
-import math
-from actvfn import*
-
-
-def derivative_sigmoid(x):
-    return (sigmoid(x) * (1- sigmoid(x)))
-
-
-def derivative_tanh(x):
-    f = 1-(tanh(x)*tanh(x))
-    return f
-
-
-def d_linear(func):
-    return np.ones(func.shape)
-
-
-def d_relu(x):
-    return np.greater(x, 0).astype(int)  
-
-
-
-def d_softmax(X):
-    x = softmax(X)
-    s = x.reshape(-1 , 1)
-    return (np.diagflat(s) - np.dot(s, np.transpose(s)))
 
 
 
