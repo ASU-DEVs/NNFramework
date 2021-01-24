36c37a8803d9557607ffb23b4b5b02a5b55bee6d
PassantElBaroudy <76267023+PassantElBaroudy@users.noreply.github.com>
2021-01-24 15:22:20 +0200

    Update actvfn.py

--- Modules/actvfn.py
+++ Modules/actvfn.py
@@ -23,6 +23,27 @@ def softmax(x):
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum(axis=0)
 
+def derivative_sigmoid(x):
+    return (sigmoid(x) * (1- sigmoid(x)))
 
+
+def derivative_tanh(x):
+    f = 1-(tanh(x)*tanh(x))
+    return f
+
+
+def d_linear(func):
+    return np.ones(func.shape)
+
+
+def d_relu(x):
+    return np.greater(x, 0).astype(int)  
+
+
+
+def d_softmax(X):
+    x = softmax(X)
+    s = x.reshape(-1 , 1)
+    return (np.diagflat(s) - np.dot(s, np.transpose(s)))
 
 
